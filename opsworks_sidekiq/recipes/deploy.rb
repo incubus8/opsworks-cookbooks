1dbeea7633cf55d3f7a9ce659252788e5cbe8683
rheza <rheza.satria.ta@gmail.com>
2017-01-25 12:42:33 +0700

    Sidekiq ignore restart when deploy

--- opsworks_sidekiq/recipes/deploy.rb
+++ opsworks_sidekiq/recipes/deploy.rb
@@ -30,9 +30,9 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
-  Chef::Log.debug("Restarting Sidekiq Application: #{application}")
-  execute "restart Rails app #{application}" do
-    command node[:sidekiq][application][:restart_command]
-  end
+  # Chef::Log.debug("Restarting Sidekiq Application: #{application}")
+  # execute "restart Rails app #{application}" do
+  #   command node[:sidekiq][application][:restart_command]
+  # end
 
 end
